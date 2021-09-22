eb21f0c7437e321faea6d37314614f924fba3512
Ryan Shihabi <ryanshihabi10@gmail.com>
2021-09-21 17:00:59 -0700

    variable rename

--- Assignments/HW2/SumConsec.py
+++ Assignments/HW2/SumConsec.py
@@ -1,16 +1,16 @@
 # grabbing intitial number
-initial = int(input("Enter a number: "))
+x = int(input("Enter a number: "))
 # This is for the recursive way
-recursive = initial
+recursive = x
 # declaring sumConsec to store result
 sumConsec = 0
 
 # execute if initial is greater than 0
-while initial > 0:
+while x > 0:
     # increment sumConsec by the initial
-    sumConsec += initial
+    sumConsec += x
     # decrement initial by one for next loop
-    initial -= 1
+    x -= 1
 
 # print the total of sumConsec
 print("Consecutive sum:", sumConsec)
@@ -18,14 +18,14 @@ print("Consecutive sum:", sumConsec)
 # ------ For fun -------
 
 # Function that takes an int parameter and returns an int
-def sumConsecRecursive(n: int) -> int:
+def sumConsecRecursive(x: int) -> int:
     # define a base case to end stack of calculations
-    if n == 1:
+    if x == 1:
         # base case returns 1
         return 1
     else:
         # calculate parameter value + the recursive sum of the number one less the parameter
-        return n + sumConsecRecursive(n-1)
+        return x + sumConsecRecursive(x-1)
 
 # print recrusive total of sum
 print("Recursive Sum:", sumConsecRecursive(recursive))
