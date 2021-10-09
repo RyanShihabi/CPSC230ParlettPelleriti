99818a12dc1529804f4be3ddbd38439866e9eba0
Ryan Shihabi <ryanshihabi10@gmail.com>
2021-10-09 16:37:06 -0700

    project 1

--- Projects_and_exams/Project1/Project1.py
+++ Projects_and_exams/Project1/Project1.py
@@ -3,17 +3,19 @@ import requests
 import json
 import random
 
+# grabbing questions and puzzle3 word from APIs
 questions = json.loads(requests.get("https://opentdb.com/api.php?amount=2&type=boolean").text)
 word = json.loads(requests.get("https://random-word-form.herokuapp.com/random/noun").text)[0]
-# print(word)
-# print(questions["results"][1]["question"])
 
+# Dictionary map for all possible choices
 d = {"n": {"north": "puzzle1", "east": "die", "south": "die", "west": "die"},
     "e": {"north": "puzzle3", "east": "die", "south": "die", "west": "die"},
     "s":{"north": "puzzle2", "east": "die", "south": "die", "west": "die"},
     "w": {"north": "puzzle4", "east": "die", "south": "die", "west": "die"}}
 
+# retrieving index for the correct question from the API
 input_count = 0
+# defining dwarfs for puzzle1
 dwarfs = ["doc", "bashful", "bneezy", "happy", "grumpy", "sleepy", "dopey"]
 
 #welcome------------------------------
@@ -25,8 +27,10 @@ direction = input(f"\nWhich direction do you want to go, {user_name}?\nN, S, E, 
 while direction not in "nsew":
     direction = input(f"Enter a valid direction\nN, S, E, or W\nEnter: ").lower()
 
+# checking first direction with dictionary
 path = d[direction]
 
+# processing first API question and filtering improper formats
 question = questions["results"][input_count]["question"]
 question = question.replace('&quot;', '"')
 question = question.replace("&#039;", "'")
@@ -36,32 +40,41 @@ user_input = input(f"\nTrue or False?\n{question}\nAnswer: ")
 
 print("\nCorrect, nice job\n") if user_input[0].lower() == answer else print("\nIncorrect, that one was tough\n")
 
+# increment to grab next question from API data
 input_count += 1
 
 #second direction---------------------
-
+# direction 2 input
 direction = input(f"Enter a new direction, {user_name}?\nNorth, South, East, or West \nEnter: ").lower()
-while direction not in "northsoutheastwest":
+while direction not in ["north", "south", "east", "west"]:
     direction = input(f"Enter a valid direction\nNorth, South, East, or West\nEnter: ").lower()
 
+# processing second API question and filtering improper formats
 question = questions["results"][input_count]["question"]
+question = question.replace('&quot;', '"')
+question = question.replace("&#039;", "'")
 answer = questions["results"][input_count]["correct_answer"][0].lower()
 
 user_input = input(f"\nTrue or False?\n{question}\nAnswer: ")
 
 print("\nCorrect, nice job\n") if user_input[0].lower() == answer else print("\nIncorrect, that one was tough\n")
 
+# checking second and final direction with dictionary
 path = path[direction]
 #---puzzles---------------------------
 if path == "puzzle1":
+    # tracking the dwarfs in an array
     selected = []
     dwarf_input = input("Name one of the seven dwarfs: ").lower()
-    while dwarf_input not in selected and dwarf_input in dwarfs:
+    # checking if input not already used, input is correct and the user has guessed two times
+    while dwarf_input not in selected and dwarf_input in dwarfs and len(selected) < 1:
         selected.append(dwarf_input)
-        dwarf_input = input("Correct! Name another one: ")
+        dwarf_input = input("Correct! Name another one: ").lower()
     else:
-        if len(selected) == 7:
-            print("you got them all, congrats")
+        # names two unique dwarfs
+        if len(selected) == 1:
+            print("you named two, congrats")
+        # two names are incorrect
         else:
             print("you got it wrong")
 
@@ -69,8 +82,10 @@ if path == "puzzle1":
 
 elif path == "puzzle2":
     print("Rolling die...")
+    # grabbing a random sumber between 1 and 6
     number = random.randint(1,6)
-    answer = input(f"Is the number {number} even or odd?").lower()
+    answer = input(f"Is the number {number} even or odd? ").lower()
+    # check answer matches with modulo of dice roll
     if (answer[0] == "e" and number % 2 == 0) or (answer[0] == "o" and number % 2 != 0):
         print("Correct! You're smart")
     else:
@@ -79,6 +94,7 @@ elif path == "puzzle2":
     print("You have made it out alive")
 
 elif path == "puzzle3":
+    # using the word from the words API
     answer = input(f"Spell {word} backwards:\n")
     if answer != word[::-1]:
         print(f"False! Answer is {word[::-1]}")
@@ -91,7 +107,7 @@ elif path == "puzzle4":
     answer = int(input("Enter a prime number: "))
 
     prime_count = 0
-    for i in range(1, 10):
+    for i in range(2, 10):
         if answer % i != 0:
             prime_count += 1
     if prime_count == 8:
