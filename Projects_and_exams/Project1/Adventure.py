8937283f7e1610db5f550dd91e26bdb5ba2e7771
Ryan Shihabi <ryanshihabi10@gmail.com>
2021-10-11 09:14:52 -0700

    puzzle 1 fix

--- Projects_and_exams/Project1/Adventure.py
+++ Projects_and_exams/Project1/Adventure.py
@@ -1,5 +1,6 @@
-import os
+# may need to install requests
 import requests
+# json unpacks api data
 import json
 import random
 
@@ -19,12 +20,16 @@ input_count = 0
 dwarfs = ["doc", "bashful", "bneezy", "happy", "grumpy", "sleepy", "dopey"]
 
 #welcome------------------------------
+# 1.
 print("Welcome")
 #choose character and opponent--------
+# 2.
 user_name = input("What is your name: ")
 #initial direction--------------------
+# 3.
 direction = input(f"\nWhich direction do you want to go, {user_name}?\nN, S, E, or W?\nEnter: ").lower()
-while direction not in "nsew":
+# 4.
+while direction[0] not in "nsew":
     direction = input(f"Enter a valid direction\nN, S, E, or W\nEnter: ").lower()
 
 # checking first direction with dictionary
@@ -36,17 +41,23 @@ question = question.replace('&quot;', '"')
 question = question.replace("&#039;", "'")
 answer = questions["results"][input_count]["correct_answer"][0].lower()
 
+# grabbing user's answer
+# 5.
 user_input = input(f"\nTrue or False?\n{question}\nAnswer: ")
 
-print("\nCorrect, nice job\n") if user_input[0].lower() == answer else print("\nIncorrect, that one was tough\n")
+# 6.
+print("\nCorrect! Nice job\n") if user_input[0].lower() == answer else print("\nIncorrect, that one was tough\n")
 
 # increment to grab next question from API data
 input_count += 1
 
 #second direction---------------------
+# 7.
 # direction 2 input
 direction = input(f"Enter a new direction, {user_name}?\nNorth, South, East, or West \nEnter: ").lower()
+# 8.
 while direction not in ["north", "south", "east", "west"]:
+    # 8.
     direction = input(f"Enter a valid direction\nNorth, South, East, or West\nEnter: ").lower()
 
 # processing second API question and filtering improper formats
@@ -55,31 +66,32 @@ question = question.replace('&quot;', '"')
 question = question.replace("&#039;", "'")
 answer = questions["results"][input_count]["correct_answer"][0].lower()
 
+# 5.
 user_input = input(f"\nTrue or False?\n{question}\nAnswer: ")
-
+# 6.
 print("\nCorrect, nice job\n") if user_input[0].lower() == answer else print("\nIncorrect, that one was tough\n")
 
 # checking second and final direction with dictionary
 path = path[direction]
 #---puzzles---------------------------
+# 10a.
 if path == "puzzle1":
     # tracking the dwarfs in an array
     selected = []
     dwarf_input = input("Name one of the seven dwarfs: ").lower()
     # checking if input not already used, input is correct and the user has guessed two times
-    while dwarf_input not in selected and dwarf_input in dwarfs and len(selected) < 1:
+    if (dwarf_input not in selected) and (dwarf_input in dwarfs):
         selected.append(dwarf_input)
         dwarf_input = input("Correct! Name another one: ").lower()
-    else:
-        # names two unique dwarfs
-        if len(selected) == 1:
+        if (dwarf_input not in selected) and (dwarf_input in dwarfs):
             print("you named two, congrats")
-        # two names are incorrect
         else:
             print("you got it wrong")
-
+    else:
+        print("you got it wrong")
+    # 11.
     print("You have made it out alive")
-
+# 10b.
 elif path == "puzzle2":
     print("Rolling die...")
     # grabbing a random sumber between 1 and 6
@@ -90,19 +102,19 @@ elif path == "puzzle2":
         print("Correct! You're smart")
     else:
         print("Incorrect")
-
+    # 11.
     print("You have made it out alive")
-
+# 10c.
 elif path == "puzzle3":
     # using the word from the words API
     answer = input(f"Spell {word} backwards:\n")
     if answer != word[::-1]:
-        print(f"False! Answer is {word[::-1]}")
+        print(f"Wrong! Answer is {word[::-1]}")
     else:
         print("Good job!")
-
+    # 11.
     print("You have made it out alive")
-
+# 10d.
 elif path == "puzzle4":
     answer = int(input("Enter a prime number: "))
 
@@ -111,10 +123,12 @@ elif path == "puzzle4":
         if answer % i != 0:
             prime_count += 1
     if prime_count == 8:
-        print("This is prime number")
+        print("This is a prime number")
     else:
         print("That is not a prime number")
+    # 11.
     print("You have made it out alive")
+# 9.
 else:
     print(f"Sorry, {user_name}, you have died")
     print("Here lies the end of your journey")
---
+++ Projects_and_exams/Project1/Project1.md
@@ -0,0 +1,11 @@
+# Project 1
+
+<p>Ryan Shihabi</p>
+<p>2391307</p>
+<p>shihabi@chapman.edu</p>
+<p>CPSC 230-08</p>
+
+### Deviations
+
+<p>Using an API for questions and backwards word</p>
+<p>Only retrieving two questions because the questions are dynamic</p>
