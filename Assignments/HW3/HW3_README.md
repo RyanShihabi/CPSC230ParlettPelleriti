6b37f290ba223afde5e8d88bfd1967d1b2307822
Ryan Shihabi <ryanshihabi10@gmail.com>
2021-10-14 12:18:31 -0700

    HW3

--- Assignments/HW3/HW3_README.md
+++ Assignments/HW3/HW3_README.md
@@ -4,3 +4,6 @@
 <p>2391307</p>
 <p>shihabi@chapman.edu</p>
 <p>CPSC 230-08</p>
+
+# Deviations
+<p>Added a while loop to confirm user input action</p>
--- Assignments/HW3/pig.py
+++ Assignments/HW3/pig.py
@@ -3,16 +3,16 @@ import random
 player = 0
 computer = 0
 
-while player < 100 and computer < 100:
+while player < 50 and computer < 50:
     print(f"\nPlayer: {player}\nComputer: {computer}\n")
 
-    print("Player's turn")
+    print("HUMAN'S TURN-----------------------------------------")
     roll = random.randint(1,6)
 
     if roll != 1:
         playerSum = 0
         playerSum += roll
-        action = input(f"\nRolled a {roll}, roll or hold (r/h)?\n")
+        action = input(f"\nRolled a {roll}\nroll or hold (r/h)?\n")
         print()
         while action[0].lower() not in "hr":
             action = input(f"\nPlease select to roll or hold the {roll} points (r/h)\n")
@@ -25,30 +25,33 @@ while player < 100 and computer < 100:
                 break
             else:
                 playerSum += roll
-                action = input(f"Rolled a {roll}, roll or hold (r/h)?\n")
+                action = input(f"Rolled a {roll}\nroll or hold (r/h)?\n")
                 print()
         player += playerSum
     else:
-        print("You have rolled a 1\n")
+        print("You have rolled a 1: No points given\n")
 
-    print("Computer's turn")
+    if player >= 50:
+        break
+
+    print("COMPUTER'S TURN--------------------------------------")
 
     roll = random.randint(1,6)
     compSum = 0
     while compSum < 20:
         roll = random.randint(1,6)
         if roll == 1:
+            print("Computer rolled a 1: No points given\n")
             compSum = 0
             break
         else:
-            if compSum + computer >= 100:
+            if compSum + computer >= 50:
                 computer += compSum
                 break
             else:
                 compSum += roll
     computer += compSum
 
-winner = "Player" if (player >= 100 and computer < 100) else "Computer"
-points = player if (player >= 100 and computer < 100) else computer
-
-print(f"\nWinner: {winner} with {points} points\n")
+winner = "Player" if (player >= 50 and computer < 50) else "Computer"
+points = player if (player >= 50 and computer < 50) else computer
+print(f"\nCongratulations {winner}!\nYou have won with {points} points!")
