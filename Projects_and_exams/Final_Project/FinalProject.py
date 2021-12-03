98c01b749c2b12411b90675a677e66644bced851
Ryan Shihabi <ryanshihabi10@gmail.com>
2021-12-02 21:58:02 -0800

    player/opponent selection

--- Projects_and_exams/Final_Project/FinalProject.py
+++ Projects_and_exams/Final_Project/FinalProject.py
@@ -1,9 +1,11 @@
 import csv
+import random
 
 player = {}
 player_list = []
-selected = []
+selected = {"Player": {}, "Opponent": {}}
 columns = []
+names = []
 
 with open("players.csv", "r") as player_file:
     player_info = csv.reader(player_file, delimiter=",")
@@ -29,5 +31,26 @@ print("Your options are\n")
 
 for person in player_list:
     print(f"{person['Name']} the {person['Class']}")
+    names.append(person["Name"])
 
-choice = input("\nWho would you like to play?")
+choice = input("\nWho would you like to play?\n")
+
+while choice not in names:
+    choice = input("\nOnly enter the name of who would you like to play?\n")
+
+
+opponent = names[random.randint(0, len(names))]
+while opponent == choice:
+    opponent = names[random.randint(0, len(names))]
+
+for person in player_list:
+    if person["Name"] == opponent:
+        selected["Opponent"] = person
+    if person["Name"] == choice:
+        selected["Player"] = person
+
+print(f"Your opponent is {selected['Opponent']['Name']} the {selected['Opponent']['Class']}")
+
+print("Here are your weapon choices:")
+
+# print(selected)
